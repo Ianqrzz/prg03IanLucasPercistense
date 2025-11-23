b96f8790d97755b783b49b56f8637efe22ae0bdb
ianep <ianep@LAPTOP-LBK04C1B>
2025-11-23 19:26:52 -0300

    Atividade 15 adicionando framework Spring ao projeto

---
+++ src/main/java/br/com/ifba/Main.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.com.ifba;
+import br.com.ifba.curso.view.CursoListar;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
+
+
+/**
+ *
+ * @author ianep
+ */
+@SpringBootApplication
+public class Main {
+    
+    
+    public static void main(String[] args) {
+        
+        ConfigurableApplicationContext context =
+                new SpringApplicationBuilder(Main.class)
+                .headless(false)
+                .run(args);
+        
+        CursoListar telaCursoSave = context.getBean(CursoListar.class);
+        telaCursoSave.setVisible(true);
+    }
+  
+}
---
+++ src/main/java/br/com/ifba/curso/entity/Curso.java
@@ -0,0 +1,72 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JDialog.java to edit this template
+ */
+package br.com.ifba.curso.entity;
+
+import br.com.ifba.curso.infrastructure.entity.PersistenceEntity;
+import jakarta.persistence.Entity; 
+import jakarta.persistence.Table;
+import java.io.Serializable;
+
+
+@Entity 
+@Table( name = "cursos")
+public class Curso extends PersistenceEntity implements Serializable {
+
+
+    /**
+     * Campo 'nome'. O JPA irá criar uma coluna chamada 'nome' no banco. Por
+     * padrão, será um VARCHAR(255).
+     */
+    private String nome;
+
+    /**
+     * Campo 'codigoCurso'. O JPA irá criar uma coluna chamada 'codigoCurso' no
+     * banco. (É uma boa prática adicionar @Column(unique = true) aqui para
+     * garantir que não haja dois cursos com o mesmo código).
+     */
+    private String codigoCurso;
+
+    /**
+     * Campo 'ativo'. O JPA irá criar uma coluna 'ativo' no banco, geralmente do
+     * tipo 'boolean' ou 'TINYINT(1)' no MySQL.
+     */
+    private boolean ativo;
+
+    /*
+     * GETTERS E SETTERS
+     *
+     * O JPA EXIGE que a classe de entidade tenha getters e setters públicos
+     * (ou um construtor padrão) para que ele possa "hidratar" (preencher)
+     * o objeto com dados do banco e ler dados do objeto para salvar no banco.
+     *
+     * 'getId' e 'setId' são usados para o ID.
+     */
+ 
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCodigoCurso() {
+        return codigoCurso;
+    }
+
+    public void setCodigoCurso(String codigoCurso) {
+        this.codigoCurso = codigoCurso;
+    }
+
+    public boolean isAtivo() {
+        return ativo;
+    }
+
+    public void setAtivo(boolean ativo) {
+        this.ativo = ativo;
+    }
+
+}
